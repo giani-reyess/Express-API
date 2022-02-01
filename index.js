2f2d057e2adf89fdb6044f8377c184443e2c1498
Giani Reyes <in4ig.seyer@gmail.com>
2022-02-01 23:40:18 +0100

    Deploy

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(errorHandler)
 
 
 // Get the server running
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
   console.log(`listening on port: ${PORT}`)
