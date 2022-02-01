18f835831c81b4ba2b008307d943f96472107861
Giani Reyes <in4ig.seyer@gmail.com>
2022-02-01 23:30:46 +0100

    cors module enhancement

--- cors/index.js
+++ cors/index.js
@@ -7,19 +7,14 @@ whiteList = [
   'http://localhost:8000'
 ]
 
-// const CORSSetting = {
-//   origin: (origin, callback) => {
-//     if (whiteList.includes(origin) || !origin) {
-//       callback(null, true)
-//     } else {
-//       callback(new Error('Not allowed'))
-//     }
-//   }
-// }
-
 const CORSSetting = {
-  origin : ['http://localhsot:3000', 'http://localhsot:8000'],
-  optionSuccessStatus: 200
+  origin: (origin, callback) => {
+    if (whiteList.includes(origin) || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed'))
+    }
+  }
 }
 
 module.exports = cors(CORSSetting)
