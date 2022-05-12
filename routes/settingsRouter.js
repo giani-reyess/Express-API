b10514b04f9be0f1c9338a1e8168faf2db543a7c
Giani Reyes <in4ig.seyer@gmail.com>
2022-05-12 11:19:53 +0200

    Configuring docker-compose.yml: db(postgres), ports, volumes(postgres source on windows)

--- routes/settingsRouter.js
+++ routes/settingsRouter.js
@@ -1,9 +1,8 @@
 const express = require('express')
-const faker = require('faker')
 const router = express.Router()
 
-router.get("/", (req,res) => {
-  res.send('This is the settings route')
+router.get("/", (req, res) => {
+    res.send('This is the settings route')
 })
 
 module.exports = router
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,9 +1,8 @@
 const express = require('express')
-const faker = require('faker')
 const router = express.Router()
 
-router.get("/", (req,res) => {
-  res.send('This is the user route')
+router.get("/", (req, res) => {
+    res.send('This is the user route')
 })
 
 module.exports = router
